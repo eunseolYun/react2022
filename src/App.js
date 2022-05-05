cec5d434b9001ae97f044eefa6fd517612e701ca
eunseolYun <skopdt73@gmail.com>
2022-05-05 19:02:24 +0900

    ~props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
-export default function App() {
+function Header(props) {
+  return (
+    <header>
+      <h1>
+        <a href="/">{props.title}</a>
+      </h1>
+    </header>
+  );
+}
+
+function Nav(props) {
+  const lis = []
+  for(let i=0; i<props.topics.length; i++){
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
+  }
+  return (
+    <nav>
+      <ol>
+        {lis}
+      </ol>
+    </nav>
+  );
+}
+
+function Article(props) {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+  );
+}
+
+function App() {
+  const topics = [
+    {id:1, title:"html", body:"html is ..."},
+    {id:2, title:"css", body:"css is ..."},
+    {id:3, title:"js", body:"js is ..."}
+  ]
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <Header title="WEB"></Header>
+      <Nav topics={topics}></Nav>
+      <Article title="Welcome" body="Hello, WEB" ></Article>
     </div>
   );
 }
+
+export default App;
